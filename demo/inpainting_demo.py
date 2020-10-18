2a32d6572bffa429236ce023056c1cdf24719378
HimariO <ron@pop-os.localdomain>
2020-10-18 14:43:14 +0800

    Inference with out dataloader

--- demo/inpainting_demo.py
+++ demo/inpainting_demo.py
@@ -1,5 +1,7 @@
+import glob
 import argparse
 
+import os
 import mmcv
 import torch
 
@@ -34,6 +36,52 @@ def main():
     if args.imshow:
         mmcv.imshow(result, 'predicted inpainting result')
 
+def parse_dir_args():
+    parser = argparse.ArgumentParser(description='Inpainting demo')
+    parser.add_argument('config', help='test config file path')
+    parser.add_argument('checkpoint', help='checkpoint file')
+    parser.add_argument('img_dir', help='path to input image file')
+    parser.add_argument('save_dir', help='path to save inpainting result')
+    parser.add_argument(
+        '--imshow', action='store_true', help='whether show image with opencv')
+    parser.add_argument('--device', type=int, default=0, help='CUDA device id')
+    args = parser.parse_args()
+    return args
+
+
+def main_dir():
+    args = parse_dir_args()
+
+    model = init_model(
+        args.config, args.checkpoint, device=torch.device('cuda', args.device))
+
+    img_and_mask = glob.glob(os.path.join(args.img_dir, '*.png'))
+    img_map = {}
+    mask_map = {}
+    for path in img_and_mask:
+        name = os.path.basename(path)
+        imgid = name.split('.')[0]
+        if '.mask' in name:
+            mask_map[imgid] = path
+        else:
+            img_map[imgid] = path
+
+    for imgid in img_map.keys():
+        masked_img_path = img_map[imgid]
+        mask_path = mask_map[imgid]
+        img_name = os.path.basename(masked_img_path)
+        
+        result = inpainting_inference(model, masked_img_path, mask_path)
+        result = tensor2img(result, min_max=(-1, 1))[..., ::-1]
+        h, w = mmcv.imread(masked_img_path).shape[:2]
+        result = result[:h, :w, ...]
+
+        save_path = os.path.join(args.save_dir, img_name)
+        mmcv.imwrite(result, save_path)
+        if args.imshow:
+            mmcv.imshow(result, 'predicted inpainting result')
+
 
 if __name__ == '__main__':
-    main()
+    # main()
+    main_dir()
